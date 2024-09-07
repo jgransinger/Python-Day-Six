5abae6221dcb19d4fd27b988b662161009bb9d23
Joe Gran <132323475+jgransinger@users.noreply.github.com>
2024-09-07 13:44:20 -0500

    Add files via upload

---
+++ Functions/task.py
@@ -0,0 +1,20 @@
+print("Hello")
+print(len("Hello"))
+
+#create function
+def new_func():
+    print("Hello")
+    print("Goodbye")
+
+#call function
+new_func()
+
+# While loops
+a = 0
+while a < 5:
+    print(a)
+    a += 1
+
+## Hurdle Challenges + Maze challenge on Reeborg's World (reeborg.ca)
+#
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%202&url=worlds%2Ftutorial_en%2Fhurdle2.json
